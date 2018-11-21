1b470146e4e57be28e609e041d298ca48618a38b
Jack-n-21 <44752360+Jack-n-21@users.noreply.github.com>
2018-11-21 23:25:57 +0200

    Add files via upload

---
+++ SERVER/data/userdata.js
@@ -0,0 +1,23 @@
+const users=[
+    {
+        id:"11",
+        name:"John",
+        email:"john@gmail.com",
+        password:"12345678"
+        
+    },{
+        id:"11",
+        name:"John",
+        email:"john@gmail.com",
+        password:"12345678"
+        
+    },
+    {
+        id:"11",
+        name:"John",
+        email:"john@gmail.com",
+        password:"12345678"
+        
+    }
+]
+export default users;
